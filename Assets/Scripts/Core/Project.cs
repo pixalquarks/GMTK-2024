af114ee209942142e33cff55c5365fa15609d1f5
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2024-08-19 04:30:58 +0900

    efficiency

--- Assets/Scripts/Core/Project.cs
+++ Assets/Scripts/Core/Project.cs
@@ -6,7 +6,10 @@ public class Project : MonoBehaviour
 {
     #region constants
     private const float BASE_DEGREDATION = 50f;
-    private const float PLANNED_PROJECT_LIFETIME = 30f;
+    private const float PLAN_LIFETIME = 30f;
+    private const float STRONG_UNDERSTAFF = 0.75f;
+    private const float DEGRADE_SPEED = 2f;
+    private const float DEGRADE_HEAL_SPEED = 3f;
     #endregion
 
     #region stats
@@ -49,11 +52,32 @@ public class Project : MonoBehaviour
     //ui fractions
     public float ProgressFraction => status == ProjectStatus.Development ? progress / requiredProgress : status == ProjectStatus.Release ? 1f : 0f;
     public float DegredationFraction => degredation / Mathf.Max(BASE_DEGREDATION, progress);
+    public float LoadFraction => currentLoad / requiredLoad;
+    public float PlanExpireFraction => degredation / PLAN_LIFETIME;
     #endregion
 
     public bool CanAddEmployee(Employee employee)
     {
         return employees.Count < maxEmployees;
+    }
+
+    /// <summary>
+    /// Progress and Revenue multiplier. Is reduced down to 0.5 depending on the strength of (weak) understaffing. Is  0 on strong understaffing.
+    /// </summary>
+    /// <returns></returns>
+    public float Efficiency()
+    {
+        if (LoadFraction < STRONG_UNDERSTAFF) return 0;
+        return 1f - Mathf.Clamp01((1 - LoadFraction) / (1 - STRONG_UNDERSTAFF)) * 0.5f;
+    }
+
+    /// <summary>
+    /// returns true on strong understaff.
+    /// Strongly understaff projects start to degrade and does not progress.
+    /// </summary>
+    public bool IsUnderstaffed()
+    {
+        return LoadFraction < STRONG_UNDERSTAFF;
     }
 
     public void AddEmployee(Employee employee)
