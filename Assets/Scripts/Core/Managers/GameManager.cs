c8bc3b0ec6cf35341addb2f0fb7a479dd5d56684
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2024-08-19 06:17:16 +0900

    project

--- Assets/Scripts/Core/Managers/GameManager.cs
+++ Assets/Scripts/Core/Managers/GameManager.cs
@@ -9,20 +9,29 @@ public class GameManager : MonoBehaviour
     /// </summary>
     public static GameManager main;
 
-    private int _money = 10000;
-    private int _quarter = 1;
+    private int money = 10000;
+    private int quarter = 1;
+
+    //game time. Different from global time.
+    private bool playing = true;
+    private bool cutscene = false;
+    private float timeScale = 1f;
 
     //do not modify directly
+    public List<Project> projects = new();
     public List<Employee> employees = new();
 
     //caclulated & cached
     /// <summary>
-    /// Calculated quarterly salary estimate. Is not guaranteed to equal actuall money consumption.
+    /// Calculated quarterly salary estimate. Is not guaranteed to equal actual money consumption.
     /// </summary>
     public int quarterlySalary;
     public int quarterlyRevenue;
 
-    public int Money => _money;
+    public int Money => money;
+    public int Quarter => quarter;
+    public bool IsPlaying => playing && !cutscene;
+    public float GameSpeed => IsPlaying ? timeScale : 0;
 
     private void Awake()
     {
@@ -31,21 +40,41 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        _quarter = 1;
+        quarter = 1;
     }
 
     public void UpdateQuarter()
     {
+        foreach (Project p in projects)
+        {
+            p.UpdateQuarter();
+        }
+
         foreach (Employee e in employees)
         {
 
             e.UpdateQuarter();
         }
+
+        if(quarter > 4)
+        {
+            //todo event
+        }
+
+        if(quarter > 7 && quarter % 4 == 1)
+        {
+            //todo revenue goal
+        }
     }
 
     public void AddMoney(int amount)
     {
-        _money += amount;
+        money += amount;
+    }
+
+    public void RemoveMoney(int amount)
+    {
+        money -= amount;
     }
 
     public void RecruitEmployee(Employee employee)
@@ -54,16 +83,27 @@ public class GameManager : MonoBehaviour
         RecalculateSalary();
     }
 
-    public void RemoveMoney(int amount)
-    {
-        _money -= amount;
-    }
-
     public void FireEmployee(Employee employee)
     {
         //note: employee must be removed from project before firing!
+        if(employee.project is not null) employee.project.RemoveEmployee(employee);
         employees.Remove(employee);
         RecalculateSalary();
+    }
+
+    public void AddProject(Project project)
+    {
+        projects.Add(project);
+    }
+
+    public void RemoveProject(Project project)
+    {
+        //assume employees are already removed
+        if (project.employees.Count > 0)
+        {
+            Debug.LogError("Error: Tried to remove a project with employees!");
+        }
+        projects.Remove(project);
     }
 
     public void RecalculateSalary()
@@ -74,5 +114,45 @@ public class GameManager : MonoBehaviour
             c += e.GetSalary();
         }
         quarterlySalary = c;
+    }
+
+    public void RecalculateRevenue()
+    {
+        int c = 0;
+        foreach (Project e in projects)
+        {
+            c += e.GetRevenue();
+        }
+        quarterlyRevenue = c;
+    }
+
+    public void OnProjectStatusChange(Project project)
+    {
+        //todo cutscene
+    }
+
+    public void StartCutscene()
+    {
+        cutscene = true;
+    }
+
+    public void EndCutscene()
+    {
+        cutscene = false;
+    }
+
+    public void Pause()
+    {
+        playing = false;
+    }
+
+    public void Resume()
+    {
+        playing = true;
+    }
+
+    public void SetSpeed(float speed)
+    {
+        timeScale = speed;
     }
 }
--- Assets/Scripts/Core/Project.cs
+++ Assets/Scripts/Core/Project.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Project : MonoBehaviour
 {
@@ -29,15 +30,22 @@ public class Project : MonoBehaviour
     #endregion
 
     #region vars
-    private ProjectStatus status = ProjectStatus.Planned;
+    private ProjectStatus status = ProjectStatus.None;
+    private ProjectStatus nextStatus = ProjectStatus.Planned;
     public readonly List<Employee> employees = new();
+    private int programmerCount = 0;
+    private int artistCount = 0;
 
     private float currentLoad; //cached
     private float degredation; //used also as a countdown in Planned projects
     private float progress; //0~100
 
+    public float currentSpeedBonus = 1f; //cached
+    public float currentRevenueBonus = 1f; //cached
+
     public enum ProjectStatus
     {
+        None, //for FSM purposes
         Planned,
         Development,
         Release,
@@ -51,10 +59,34 @@ public class Project : MonoBehaviour
 
     //ui fractions
     public float ProgressFraction => status == ProjectStatus.Development ? progress / requiredProgress : status == ProjectStatus.Release ? 1f : 0f;
-    public float DegredationFraction => degredation / Mathf.Max(BASE_DEGREDATION, progress);
+    public float DegredationFraction => degredation / Mathf.Max(BASE_DEGREDATION, progress / requiredProgress * 100f);
     public float LoadFraction => currentLoad / requiredLoad;
     public float PlanExpireFraction => degredation / PLAN_LIFETIME;
     #endregion
+
+    #region events
+    public StatusUpdateEvent onStatusChanged = new();
+    public EmployeeUpdateEvent onEmployeeChanged = new();
+
+    [System.Serializable]
+    public class StatusUpdateEvent : UnityEvent<ProjectStatus> { }
+
+    [System.Serializable]
+    public class EmployeeUpdateEvent : UnityEvent { }
+    #endregion
+
+    public void UpdateQuarter()
+    {
+        if(status == ProjectStatus.Release)
+        {
+            GameManager.main.AddMoney(GetRevenue());
+        }
+    }
+
+    public int GetRevenue()
+    {
+        return Mathf.RoundToInt(baseRevenue * currentRevenueBonus * Efficiency());
+    }
 
     public bool CanAddEmployee(Employee employee)
     {
@@ -68,6 +100,7 @@ public class Project : MonoBehaviour
     public float Efficiency()
     {
         if (LoadFraction < STRONG_UNDERSTAFF) return 0;
+        if (LoadFraction >= 1) return 1;
         return 1f - Mathf.Clamp01((1 - LoadFraction) / (1 - STRONG_UNDERSTAFF)) * 0.5f;
     }
 
@@ -77,15 +110,24 @@ public class Project : MonoBehaviour
     /// </summary>
     public bool IsUnderstaffed()
     {
-        return LoadFraction < STRONG_UNDERSTAFF;
+        return LoadFraction < STRONG_UNDERSTAFF || programmerCount < minProgrammerCount || artistCount < minArtistCount;
+    }
+
+    public bool ShouldBeScrapped()
+    {
+        return degredation >= Mathf.Max(BASE_DEGREDATION, progress / requiredProgress * 100f) && IsUnderstaffed();
     }
 
     public void AddEmployee(Employee employee)
     {
         employees.Add(employee);
         employee.project = this;
+
+        if (employee.role == Employee.EmployeeRole.Programmer) programmerCount++;
+        else artistCount++;
         RecalculateLoad();
         employee.OnProjectJoined();
+        onEmployeeChanged.Invoke();
     }
 
     public void RemoveEmployee(Employee employee)
@@ -93,15 +135,111 @@ public class Project : MonoBehaviour
         employee.OnProjectLeft();
         employees.Remove(employee);
         employee.project = null;
+
+        if (employee.role == Employee.EmployeeRole.Programmer) programmerCount--;
+        else artistCount--;
         RecalculateLoad();
+        onEmployeeChanged.Invoke();
     }
 
     public void RecalculateLoad()
     {
         currentLoad = 0f;
+        currentSpeedBonus = 1f;
+        currentRevenueBonus = 1f;
         foreach (Employee e in employees)
         {
             currentLoad += 1 + e.SkillAbility * 0.1f;
+            currentSpeedBonus += e.SkillSpeed * 0.05f;
+            currentRevenueBonus += e.SkillPassion * 0.05f;
+        }
+
+        GameManager.main.RecalculateRevenue();
+    }
+
+    private void Kill()
+    {
+        GameManager.main.RemoveProject(this);
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if (!GameManager.main.IsPlaying) return;
+        float delta = Time.deltaTime * GameManager.main.GameSpeed;
+
+        //auto state change
+        if(nextStatus == ProjectStatus.None)
+        {
+            switch (status)
+            {
+                case ProjectStatus.Planned:
+                    if (employees.Count > 0) nextStatus = ProjectStatus.Development;
+                    else if (degredation >= PLAN_LIFETIME) nextStatus = ProjectStatus.Scrapped;
+                    break;
+                case ProjectStatus.Development:
+                    if (ShouldBeScrapped()) nextStatus = ProjectStatus.Scrapped;
+                    else if (progress >= requiredProgress) nextStatus = ProjectStatus.Release;
+                    break;
+                case ProjectStatus.Release:
+                    if (ShouldBeScrapped()) nextStatus = ProjectStatus.Scrapped;
+                    break;
+            }
+        }
+
+        //state init
+        if (nextStatus != ProjectStatus.None)
+        {
+            status = nextStatus;
+            RecalculateLoad();
+            switch (status)
+            {
+                case ProjectStatus.Planned:
+                    degredation = 0;
+                    break;
+                case ProjectStatus.Development:
+                    GameManager.main.RemoveMoney(initialCost);
+                    degredation = 0;
+                    break;
+                case ProjectStatus.Release:
+                    progress = requiredProgress;
+                    //todo stat change on release
+                    break;
+                case ProjectStatus.Scrapped:
+                    break;
+            }
+            onStatusChanged.Invoke(status);
+            GameManager.main.OnProjectStatusChange(this);
+        }
+
+        //state update
+        switch (status)
+        {
+            case ProjectStatus.Planned:
+                degredation += delta;
+                break;
+            case ProjectStatus.Development:
+            case ProjectStatus.Release:
+                if (IsUnderstaffed())
+                {
+                    degredation += delta * DEGRADE_SPEED;
+                }
+                else
+                {
+                    if(status == ProjectStatus.Development) progress += delta * Efficiency();
+                    if (degredation > 0)
+                    {
+                        degredation -= delta * DEGRADE_HEAL_SPEED;
+                        if (degredation < 0) degredation = 0;
+                    }
+                }
+                break;
+            case ProjectStatus.Scrapped:
+                if (employees.Count < 0)
+                {
+                    Kill();
+                }
+                break;
         }
     }
 }
