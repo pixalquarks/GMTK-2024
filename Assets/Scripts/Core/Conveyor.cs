cadf1ae40d84e173c4ef2010a09ec00a6d707881
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2024-08-21 01:43:18 +0900

    aaaaaaaaa

--- Assets/Scripts/Core/Conveyor.cs
+++ Assets/Scripts/Core/Conveyor.cs
@@ -14,9 +14,13 @@ public class Conveyor : MonoBehaviour {
     [SerializeField] private Transform basketParent;
     [SerializeField] private EmployeeBasket basketPrefab;
     [SerializeField] private EmployeeGenerator generator;
+    [SerializeField] private Transform beltParent;
+    [SerializeField] private int belts = 9;
+    [SerializeField] private Vector2 beltSize = new Vector2(3.5f, 4f);
 
     private float time;
     private float nextSpawnTime;
+    private float beltProgression;
 
     private void Start() {
         nextSpawnTime = PollNextSpawnTime();
@@ -31,10 +35,39 @@ public class Conveyor : MonoBehaviour {
         float delta = Time.deltaTime * GameManager.main.GameSpeed;
 
         time += delta;
-        if(time > nextSpawnTime) {
+        beltProgression += delta * speed / (endPoint.position.x - startPoint.position.x);
+        beltProgression %= 1f;
+        UpdateBelts();
+        if (time > nextSpawnTime) {
             nextSpawnTime = PollNextSpawnTime();
             time = 0;
             Spawn();
+        }
+    }
+
+    private void UpdateBelts()
+    {
+        float dst = endPoint.position.x - startPoint.position.x;
+        float d = 1f / belts;
+        Debug.Log("dst "+ dst);
+        for (int i = 0; i < belts; i++)
+        {
+            float offset = (d * i + beltProgression) % 1f;
+            Debug.Log($"{i} : {offset}");
+            Transform t = beltParent.GetChild(i);
+            t.transform.position = startPoint.position + Vector3.right * offset * dst;
+            if(offset < d * 0.5f)
+            {
+                t.localScale = new Vector3(offset / d * 2f * beltSize.x, beltSize.y, 1f);
+            }
+            else if(offset > 1f - d * 0.5f)
+            {
+                t.localScale = new Vector3((1f - offset) / d * 2f * beltSize.x, beltSize.y, 1f);
+            }
+            else
+            {
+                t.localScale = beltSize;
+            }
         }
     }
 
